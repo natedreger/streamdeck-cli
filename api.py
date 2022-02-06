cf5469996b700a266b2154b73c63659925163606
natedreger <info@natedreger.com>
2022-02-06 11:22:43 -0600

    functions

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from typing import Dict, Tuple, Union, cast
 from warnings import warn
 
 from PIL import Image, ImageDraw, ImageFont
-from PySide2.QtCore import Signal
+from PySide2.QtCore import QObject, Signal
 from StreamDeck import DeviceManager
 from StreamDeck.Devices import StreamDeck
 from StreamDeck.ImageHelpers import PILHelper
@@ -21,7 +21,7 @@ streamdecks_lock = threading.Lock()
 key_event_lock = threading.Lock()
 
 
-class KeySignalEmitter():
+class KeySignalEmitter(QObject):
     key_pressed = Signal(str, int, bool)
 
 
@@ -37,6 +37,17 @@ def _key_change_callback(deck_id: str, _deck: StreamDeck.StreamDeck, key: int, s
     # shared state with a lock
     with key_event_lock:
         streamdesk_keys.key_pressed.emit(deck_id, key, state)
+
+
+def get_display_timeout(deck_id: str) -> int:
+    """ Returns the amount of time in seconds before the display gets dimmed."""
+    return cast(int, state.get(deck_id, {}).get("display_timeout", 0))
+
+
+def set_display_timeout(deck_id: str, timeout: int) -> None:
+    """ Sets the amount of time in seconds before the display gets dimmed."""
+    state.setdefault(deck_id, {})["display_timeout"] = timeout
+    _save_state()
 
 
 def _save_state():
@@ -141,6 +152,77 @@ def _button_state(deck_id: str, page: int, button: int) -> dict:
     buttons_state = buttons.setdefault(page, {})  # type: ignore
     return buttons_state.setdefault(button, {})  # type: ignore
 
+
+def swap_buttons(deck_id: str, page: int, source_button: int, target_button: int) -> None:
+    """Swaps the properties of the source and target buttons"""
+    temp = cast(dict, state[deck_id]["buttons"])[page][source_button]
+    cast(dict, state[deck_id]["buttons"])[page][source_button] = cast(
+        dict, state[deck_id]["buttons"]
+    )[page][target_button]
+    cast(dict, state[deck_id]["buttons"])[page][target_button] = temp
+
+    # Clear the cache so images will be recreated on render
+    image_cache.pop(f"{deck_id}.{page}.{source_button}", None)
+    image_cache.pop(f"{deck_id}.{page}.{target_button}", None)
+
+    _save_state()
+    render()
+
+
+def set_button_text(deck_id: str, page: int, button: int, text: str) -> None:
+    """Set the text associated with a button"""
+    if get_button_text(deck_id, page, button) != text:
+        _button_state(deck_id, page, button)["text"] = text
+        image_cache.pop(f"{deck_id}.{page}.{button}", None)
+        render()
+        _save_state()
+
+
+def get_button_text(deck_id: str, page: int, button: int) -> str:
+    """Returns the text set for the specified button"""
+    return _button_state(deck_id, page, button).get("text", "")
+
+
+def set_button_icon(deck_id: str, page: int, button: int, icon: str) -> None:
+    """Sets the icon associated with a button"""
+    if get_button_icon(deck_id, page, button) != icon:
+        _button_state(deck_id, page, button)["icon"] = icon
+        image_cache.pop(f"{deck_id}.{page}.{button}", None)
+        render()
+        _save_state()
+
+
+def get_button_icon(deck_id: str, page: int, button: int) -> str:
+    """Returns the icon set for a particular button"""
+    return _button_state(deck_id, page, button).get("icon", "")
+
+
+def set_button_change_brightness(deck_id: str, page: int, button: int, amount: int) -> None:
+    """Sets the brightness changing associated with a button"""
+    if get_button_change_brightness(deck_id, page, button) != amount:
+        _button_state(deck_id, page, button)["brightness_change"] = amount
+        render()
+        _save_state()
+
+
+def get_button_change_brightness(deck_id: str, page: int, button: int) -> int:
+    """Returns the brightness change set for a particular button"""
+    return _button_state(deck_id, page, button).get("brightness_change", 0)
+
+
+def set_button_command(deck_id: str, page: int, button: int, command: str) -> None:
+    """Sets the command associated with the button"""
+    if get_button_command(deck_id, page, button) != command:
+        _button_state(deck_id, page, button)["command"] = command
+        _save_state()
+
+#ND Add
+def set_button_command_type(deck_id: str, page: int, button: int, command_type: str) -> None:
+    """Sets the command associated with the button"""
+    if get_button_command_type(deck_id, page, button) != command_type:
+        _button_state(deck_id, page, button)["command_type"] = command_type
+        _save_state()
+
 def get_button_command(deck_id: str, page: int, button: int) -> str:
     """Returns the command set for the specified button"""
     return _button_state(deck_id, page, button).get("command", "")
@@ -153,10 +235,96 @@ def get_button_command_type(deck_id: str, page: int, button: int) -> str:
         type = 'Command Type'
     return type
 
-#
+
+def set_button_switch_page(deck_id: str, page: int, button: int, switch_page: int) -> None:
+    """Sets the page switch associated with the button"""
+    if get_button_switch_page(deck_id, page, button) != switch_page:
+        _button_state(deck_id, page, button)["switch_page"] = switch_page
+        _save_state()
+
+
+def get_button_switch_page(deck_id: str, page: int, button: int) -> int:
+    """Returns the page switch set for the specified button. 0 implies no page switch."""
+    return _button_state(deck_id, page, button).get("switch_page", 0)
+
+
+def set_button_keys(deck_id: str, page: int, button: int, keys: str) -> None:
+    """Sets the keys associated with the button"""
+    if get_button_keys(deck_id, page, button) != keys:
+        _button_state(deck_id, page, button)["keys"] = keys
+        _save_state()
+
+#ND Add
+def set_button_command_string(deck_id: str, page: int, button: int, command_string: str) -> None:
+    """Sets the keys associated with the button"""
+    if get_button_command_string(deck_id, page, button) != command_string:
+        _button_state(deck_id, page, button)["command_string"] = command_string
+        _save_state()
+
+def get_button_keys(deck_id: str, page: int, button: int) -> str:
+    """Returns the keys set for the specified button"""
+    return _button_state(deck_id, page, button).get("keys", "")
+
+#ND Add
+def get_button_command_string(deck_id: str, page: int, button: int) -> str:
+    """Returns the command_string set for the specified button"""
+    return _button_state(deck_id, page, button).get("command_string", "")
+
+
+def set_button_write(deck_id: str, page: int, button: int, write: str) -> None:
+    """Sets the text meant to be written when button is pressed"""
+    if get_button_write(deck_id, page, button) != write:
+        _button_state(deck_id, page, button)["write"] = write
+        _save_state()
+
+
+def get_button_write(deck_id: str, page: int, button: int) -> str:
+    """Returns the text to be produced when the specified button is pressed"""
+    return _button_state(deck_id, page, button).get("write", "")
+
+
+def set_brightness(deck_id: str, brightness: int) -> None:
+    """Sets the brightness for every button on the deck"""
+    if get_brightness(deck_id) != brightness:
+        decks[deck_id].set_brightness(brightness)
+        state.setdefault(deck_id, {})["brightness"] = brightness
+        _save_state()
+
+
+def get_brightness(deck_id: str) -> int:
+    """Gets the brightness that is set for the specified stream deck"""
+    return state.get(deck_id, {}).get("brightness", 100)  # type: ignore
+
+
+def get_brightness_dimmed(deck_id: str) -> int:
+    """Gets the percentage value of the full brightness that is used when dimming the specified
+    stream deck"""
+    return state.get(deck_id, {}).get("brightness_dimmed", 0)  # type: ignore
+
+
+def set_brightness_dimmed(deck_id: str, brightness_dimmed: int) -> None:
+    """Sets the percentage value that will be used for dimming the full brightness"""
+    state.setdefault(deck_id, {})["brightness_dimmed"] = brightness_dimmed
+    _save_state()
+
+
+def change_brightness(deck_id: str, amount: int = 1) -> None:
+    """Change the brightness of the deck by the specified amount"""
+    set_brightness(deck_id, max(min(get_brightness(deck_id) + amount, 100), 0))
+
+
 def get_page(deck_id: str) -> int:
     """Gets the current page shown on the stream deck"""
     return state.get(deck_id, {}).get("page", 0)  # type: ignore
+
+
+def set_page(deck_id: str, page: int) -> None:
+    """Sets the current page shown on the stream deck"""
+    if get_page(deck_id) != page:
+        state.setdefault(deck_id, {})["page"] = page
+        render()
+        _save_state()
+
 
 def render(decks) -> None:
     """renders all decks"""
--- streamdeck-cli.py
+++ streamdeck-cli.py
@@ -11,20 +11,123 @@
 # tiles generated at runtime, and responding to button state change events.
 
 import os
+import sys
+import json
 import threading
+from subprocess import Popen
+import shlex
 
 import api
 from PIL import Image, ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
 from StreamDeck.Devices import StreamDeck
-from typing import Dict, Tuple, Union, cast
+from typing import Dict, Tuple, Union, cast, Callable
 
 decks: Dict[str, StreamDeck.StreamDeck] = {}
 
 # Folder location of image assets used by this example.
 ASSETS_PATH = os.path.join(os.path.dirname(__file__), "Assets")
 
+class Dimmer:
+    timeout = 0
+    brightness = -1
+    brightness_dimmed = -1
+    __stopped = False
+    __dimmer_brightness = -1
+    __timer = None
+    __change_timer = None
+
+    def __init__(
+        self,
+        timeout: int,
+        brightness: int,
+        brightness_dimmed: int,
+        brightness_callback: Callable[[int], None],
+    ):
+        """ Constructs a new Dimmer instance
+
+        :param int timeout: The time in seconds before the dimmer starts.
+        :param int brightness: The normal brightness level.
+        :param Callable[[int], None] brightness_callback: Callback that receives the current
+                                                          brightness level.
+         """
+        self.timeout = timeout
+        self.brightness = brightness
+        self.brightness_dimmed = brightness_dimmed
+        self.brightness_callback = brightness_callback
+
+    def stop(self) -> None:
+        """ Stops the dimmer and sets the brightness back to normal. Call
+        reset to start normal dimming operation. """
+        if self.__timer:
+            self.__timer.stop()
+
+        if self.__change_timer:
+            self.__change_timer.stop()
+
+        self.__dimmer_brightness = self.brightness
+        self.brightness_callback(self.brightness)
+        self.__stopped = True
+
+    def reset(self) -> bool:
+        """ Reset the dimmer and start counting down again. If it was busy dimming, it will
+        immediately stop dimming. Callback fires to set brightness back to normal."""
+
+        self.__stopped = False
+        if self.__timer:
+            self.__timer.stop()
+
+        if self.__change_timer:
+            self.__change_timer.stop()
+
+        if self.timeout:
+            self.__timer = QTimer()
+            self.__timer.setSingleShot(True)
+            self.__timer.timeout.connect(partial(self.change_brightness))
+            self.__timer.start(self.timeout * 1000)
+
+        if self.__dimmer_brightness != self.brightness:
+            previous_dimmer_brightness = self.__dimmer_brightness
+            self.brightness_callback(self.brightness)
+            self.__dimmer_brightness = self.brightness
+            if previous_dimmer_brightness < 10:
+                return True
+
+        return False
+
+    def dim(self, toggle: bool = False):
+        """ Manually initiate a dim event.
+            If the dimmer is stopped, this has no effect. """
+
+        if self.__stopped:
+            return
+
+        if toggle and self.__dimmer_brightness == 0:
+            self.reset()
+        elif self.__timer and self.__timer.isActive():
+            # No need for the timer anymore, stop it
+            self.__timer.stop()
+
+            # Verify that we're not already at the target brightness nor
+            # busy with dimming already
+            if self.__change_timer is None and self.__dimmer_brightness:
+                self.change_brightness()
+
+    def change_brightness(self):
+        """ Move the brightness level down by one and schedule another change_brightness event. """
+        if self.__dimmer_brightness and self.__dimmer_brightness >= self.brightness_dimmed:
+            self.__dimmer_brightness = self.__dimmer_brightness - 1
+            self.brightness_callback(self.__dimmer_brightness)
+            self.__change_timer = QTimer()
+            self.__change_timer.setSingleShot(True)
+            self.__change_timer.timeout.connect(partial(self.change_brightness))
+            self.__change_timer.start(10)
+        else:
+            self.__change_timer = None
+
+
+dimmers: Dict[str, Dimmer] = {}
 
 # Generates a custom tile with run-time generated text and custom image via the
 # PIL module.
@@ -70,12 +173,17 @@ def get_key_style(deck, key, state):
 
 # Creates a new key image based on the key index, style and current key state
 # and updates the image on the StreamDeck.
-def update_key_image(deck, key, state):
+def update_key_image(deck, page, key, state):
+    deck_id = deck.get_serial_number()
     # Determine what icon and label to use on the generated key.
     key_style = get_key_style(deck, key, state)
 
     # Generate the custom key with the requested image and label.
-    image = render_key_image(deck, key_style["icon"], key_style["font"], key_style["label"])
+    icon = api.get_button_icon(deck_id, page, key)
+    if icon == '':
+        icon = key_style['icon']
+    label = api.get_button_text(deck_id, page, key)
+    image = render_key_image(deck, icon, key_style["font"], label)
 
     # Use a scoped-with on the deck to ensure we're the only thread using it
     # right now.
@@ -87,26 +195,140 @@ def update_key_image(deck, key, state):
 # Prints key state change information, updates rhe key image and performs any
 # associated actions when a key is pressed.
 def key_change_callback(deck, key, state):
-    # Print new key state
-    print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
-
-    # Update the key image based on the new key state.
-    update_key_image(deck, key, state)
-
-    # Check if the key is changing to the pressed state.
+    deck_id = deck.get_serial_number()
+    page = api.get_page(deck_id)
+    update_key_image(deck, page, key, state)
+    internal_command = {}
+    external_command = {}
+    external_commands = ['OSC', 'MIDI', 'MQTT']
+    print(deck_id, key, state)
     if state:
-        key_style = get_key_style(deck, key, state)
+        # if dimmers[deck_id].reset():
+        #     return
 
-        # When an exit button is pressed, close the application.
-        if key_style["name"] == "exit":
-            # Use a scoped-with on the deck to ensure we're the only thread
-            # using it right now.
-            with deck:
-                # Reset deck, clearing all button images.
-                deck.reset()
 
-                # Close deck handle, terminating internal worker threads.
-                deck.close()
+        if api.get_button_command_type(deck_id, page, key) in external_commands:
+            external_command = {"command_type":api.get_button_command_type(deck_id, page, key), "command_string":api.get_button_command_string(deck_id, page, key)}
+        else:
+            internal_command = {"command_type":api.get_button_command_type(deck_id, page, key), "command_string":api.get_button_command_string(deck_id, page, key)}
+
+        if external_command:
+            # queue.put(external_command)
+            # print(f"External: {external_command}")
+            print(json.dumps(external_command), file = sys.stdout)
+
+        if internal_command:
+            # print(f"Internal: {internal_command}")
+
+            command = internal_command['command_type'] == 'Command'
+            if command:
+                try:
+                    Popen(shlex.split(internal_command['command_string']))
+                except Exception as error:
+                    print(f"The command '{internal_command['command_string']}' failed: {error}")
+
+            keys = internal_command['command_type'] == 'Keystroke'
+            if keys:
+                keys = internal_command['command_string']
+                keys = keys.strip().replace(" ", "")
+                for section in keys.split(","):
+                    # Since + and , are used to delimit our section and keys to press,
+                    # they need to be substituted with keywords.
+                    section_keys = [_replace_special_keys(key_name) for key_name in section.split("+")]
+
+                    # Translate string to enum, or just the string itself if not found
+                    section_keys = [
+                        getattr(Key, key_name.lower(), key_name) for key_name in section_keys
+                    ]
+
+                    for key_name in section_keys:
+                        if isinstance(key_name, str) and key_name.startswith("delay"):
+                            sleep_time_arg = key_name.split("delay", 1)[1]
+                            if sleep_time_arg:
+                                try:
+                                    sleep_time = float(sleep_time_arg)
+                                except Exception:
+                                    print(f"Could not convert sleep time to float '{sleep_time_arg}'")
+                                    sleep_time = 0
+                            else:
+                                # default if not specified
+                                sleep_time = 0.5
+
+                            if sleep_time:
+                                try:
+                                    time.sleep(sleep_time)
+                                except Exception:
+                                    print(f"Could not sleep with provided sleep time '{sleep_time}'")
+                        else:
+                            try:
+                                keyboard.press(key_name)
+                            except Exception:
+                                print(f"Could not press key '{key_name}'")
+
+                    for key_name in section_keys:
+                        if not (isinstance(key_name, str) and key_name.startswith("delay")):
+                            try:
+                                keyboard.release(key_name)
+                            except Exception:
+                                print(f"Could not release key '{key_name}'")
+
+            write = internal_command['command_type'] == "Text"
+            if write:
+                try:
+                    keyboard.type(internal_command['command_string'])
+                except Exception as error:
+                    print(f"Could not complete the write command: {error}")
+
+            # Set absolute brightness
+            set_brightness = internal_command['command_type'] == 'Set Brightness'
+            if set_brightness:
+                try:
+                    api.set_brightness(deck_id, int(internal_command['command_string']))
+                    dimmers[deck_id].brightness = api.get_brightness(deck_id)
+                    dimmers[deck_id].reset()
+                except Exception as error:
+                    print(f"Could not change brightness: {error}")
+
+            # Dim by percentage
+            change_brightness = internal_command['command_type'] == 'Brightness'
+            if set_brightness:
+                try:
+                    api.change_brightness(deck_id, int(internal_command['command_string']))
+                    dimmers[deck_id].brightness = api.get_brightness(deck_id)
+                    dimmers[deck_id].reset()
+                except Exception as error:
+                    print(f"Could not change brightness: {error}")
+
+            switch_page = internal_command['command_type'] == 'Page'
+            if switch_page:
+                api.set_page(deck_id, int(internal_command['command_string']) - 1)
+
+            CloseStreamDeck = internal_command['command_type'] == 'CloseStreamDeck'
+            if CloseStreamDeck:
+                api.close_decks()
+                sys.exit()
+# # #
+# def key_change_callback(deck, key, state):
+#     # Print new key state
+#     print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
+#
+#     # Update the key image based on the new key state.
+#     update_key_image(deck, key, state)
+#
+#     # Check if the key is changing to the pressed state.
+#     if state:
+#         key_style = get_key_style(deck, key, state)
+#
+#         # When an exit button is pressed, close the application.
+#         if key_style["name"] == "exit":
+#             # Use a scoped-with on the deck to ensure we're the only thread
+#             # using it right now.
+#             with deck:
+#                 # Reset deck, clearing all button images.
+#                 deck.reset()
+#
+#                 # Close deck handle, terminating internal worker threads.
+#                 deck.close()
 
 
 if __name__ == "__main__":
@@ -127,10 +349,6 @@ if __name__ == "__main__":
 
         # Set initial screen brightness to 30%.
         deck.set_brightness(30)
-
-        # Set initial key images.
-        for key in range(deck.key_count()):
-            update_key_image(deck, key, False)
 
         # Register callback function for when a key state changes.
         deck.set_key_callback(key_change_callback)
