b3c0e0fcdcf2ec3fc67d023e3477654f027f8260
natedreger <info@natedreger.com>
2022-02-06 09:26:58 -0600

    start again

--- streamdeck-cli.py
+++ streamdeck-cli.py
@@ -1,143 +1,136 @@
 #!./venv/bin/python3
 
-"""Defines the QT powered interface for configuring Stream Decks"""
+#         Python Stream Deck Library
+#      Released under the MIT license
+#
+#   dean [at] fourwalledcubicle [dot] com
+#         www.fourwalledcubicle.com
+#
+
+# Example script showing basic library usage - updating key images with new
+# tiles generated at runtime, and responding to button state change events.
+
 import os
-import shlex
-import sys
-import time
-import json
-from functools import partial
-from subprocess import Popen  # nosec - Need to allow users to specify arbitrary commands
+import threading
 
-import api
-
+from PIL import Image, ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
+from StreamDeck.ImageHelpers import PILHelper
 
-# ND Add
-# Physical Device
-def handle_keypress(deck_id: str, key: int, state: bool) -> None:
-    internal_command = {}
-    external_command = {}
-    external_commands = ['OSC', 'MIDI', 'MQTT']
+# Folder location of image assets used by this example.
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), "Assets")
+
+
+# Generates a custom tile with run-time generated text and custom image via the
+# PIL module.
+def render_key_image(deck, icon_filename, font_filename, label_text):
+    # Resize the source image asset to best-fit the dimensions of a single key,
+    # leaving a margin at the bottom so that we can draw the key title
+    # afterwards.
+    icon = Image.open(icon_filename)
+    image = PILHelper.create_scaled_image(deck, icon, margins=[0, 0, 20, 0])
+
+    # Load a custom TrueType font and use it to overlay the key index, draw key
+    # label onto the image a few pixels from the bottom of the key.
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(font_filename, 14)
+    draw.text((image.width / 2, image.height - 5), text=label_text, font=font, anchor="ms", fill="white")
+
+    return PILHelper.to_native_format(deck, image)
+
+
+# Returns styling information for a key based on its position and state.
+def get_key_style(deck, key, state):
+    # Last button in the example application is the exit button.
+    exit_key_index = deck.key_count() - 1
+
+    if key == exit_key_index:
+        name = "exit"
+        icon = "{}.png".format("Exit")
+        font = "Roboto-Regular.ttf"
+        label = "Bye" if state else "Exit"
+    else:
+        name = "emoji"
+        icon = "{}.png".format("Pressed" if state else "Released")
+        font = "Roboto-Regular.ttf"
+        label = "Pressed!" if state else "Key {}".format(key)
+
+    return {
+        "name": name,
+        "icon": os.path.join(ASSETS_PATH, icon),
+        "font": os.path.join(ASSETS_PATH, font),
+        "label": label
+    }
+
+
+# Creates a new key image based on the key index, style and current key state
+# and updates the image on the StreamDeck.
+def update_key_image(deck, key, state):
+    # Determine what icon and label to use on the generated key.
+    key_style = get_key_style(deck, key, state)
+
+    # Generate the custom key with the requested image and label.
+    image = render_key_image(deck, key_style["icon"], key_style["font"], key_style["label"])
+
+    # Use a scoped-with on the deck to ensure we're the only thread using it
+    # right now.
+    with deck:
+        # Update requested key with the generated image.
+        deck.set_key_image(key, image)
+
+
+# Prints key state change information, updates rhe key image and performs any
+# associated actions when a key is pressed.
+def key_change_callback(deck, key, state):
+    # Print new key state
+    print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
+
+    # Update the key image based on the new key state.
+    update_key_image(deck, key, state)
+
+    # Check if the key is changing to the pressed state.
     if state:
-        if dimmers[deck_id].reset():
-            return
+        key_style = get_key_style(deck, key, state)
 
-        keyboard = Controller()
-        page = api.get_page(deck_id)
+        # When an exit button is pressed, close the application.
+        if key_style["name"] == "exit":
+            # Use a scoped-with on the deck to ensure we're the only thread
+            # using it right now.
+            with deck:
+                # Reset deck, clearing all button images.
+                deck.reset()
 
-        if api.get_button_command_type(deck_id, page, key) in external_commands:
-            external_command = {"command_type":api.get_button_command_type(deck_id, page, key), "command_string":api.get_button_command_string(deck_id, page, key)}
-        else:
-            internal_command = {"command_type":api.get_button_command_type(deck_id, page, key), "command_string":api.get_button_command_string(deck_id, page, key)}
+                # Close deck handle, terminating internal worker threads.
+                deck.close()
 
-        if external_command:
-            # queue.put(external_command)
-            # print(f"External: {external_command}")
-            print(json.dumps(external_command), file = sys.stdout)
-
-        if internal_command:
-            # print(f"Internal: {internal_command}")
-
-            command = internal_command['command_type'] == 'Command'
-            if command:
-                try:
-                    Popen(shlex.split(internal_command['command_string']))
-                except Exception as error:
-                    print(f"The command '{internal_command['command_string']}' failed: {error}")
-
-            keys = internal_command['command_type'] == 'Keystroke'
-            if keys:
-                keys = internal_command['command_string']
-                keys = keys.strip().replace(" ", "")
-                for section in keys.split(","):
-                    # Since + and , are used to delimit our section and keys to press,
-                    # they need to be substituted with keywords.
-                    section_keys = [_replace_special_keys(key_name) for key_name in section.split("+")]
-
-                    # Translate string to enum, or just the string itself if not found
-                    section_keys = [
-                        getattr(Key, key_name.lower(), key_name) for key_name in section_keys
-                    ]
-
-                    for key_name in section_keys:
-                        if isinstance(key_name, str) and key_name.startswith("delay"):
-                            sleep_time_arg = key_name.split("delay", 1)[1]
-                            if sleep_time_arg:
-                                try:
-                                    sleep_time = float(sleep_time_arg)
-                                except Exception:
-                                    print(f"Could not convert sleep time to float '{sleep_time_arg}'")
-                                    sleep_time = 0
-                            else:
-                                # default if not specified
-                                sleep_time = 0.5
-
-                            if sleep_time:
-                                try:
-                                    time.sleep(sleep_time)
-                                except Exception:
-                                    print(f"Could not sleep with provided sleep time '{sleep_time}'")
-                        else:
-                            try:
-                                keyboard.press(key_name)
-                            except Exception:
-                                print(f"Could not press key '{key_name}'")
-
-                    for key_name in section_keys:
-                        if not (isinstance(key_name, str) and key_name.startswith("delay")):
-                            try:
-                                keyboard.release(key_name)
-                            except Exception:
-                                print(f"Could not release key '{key_name}'")
-
-            write = internal_command['command_type'] == "Text"
-            if write:
-                try:
-                    keyboard.type(internal_command['command_string'])
-                except Exception as error:
-                    print(f"Could not complete the write command: {error}")
-
-            # Set absolute brightness
-            set_brightness = internal_command['command_type'] == 'Set Brightness'
-            if set_brightness:
-                try:
-                    api.set_brightness(deck_id, int(internal_command['command_string']))
-                    dimmers[deck_id].brightness = api.get_brightness(deck_id)
-                    dimmers[deck_id].reset()
-                except Exception as error:
-                    print(f"Could not change brightness: {error}")
-
-            # Dim by percentage
-            change_brightness = internal_command['command_type'] == 'Brightness'
-            if set_brightness:
-                try:
-                    api.change_brightness(deck_id, int(internal_command['command_string']))
-                    dimmers[deck_id].brightness = api.get_brightness(deck_id)
-                    dimmers[deck_id].reset()
-                except Exception as error:
-                    print(f"Could not change brightness: {error}")
-
-            switch_page = internal_command['command_type'] == 'Page'
-            if switch_page:
-                api.set_page(deck_id, int(internal_command['command_string']) - 1)
-
-            CloseStreamDeck = internal_command['command_type'] == 'CloseStreamDeck'
-            if CloseStreamDeck:
-                api.close_decks()
-                sys.exit()
-
-def start() -> None:
-    # api.streamdesk_keys.key_pressed.connect(handle_keypress)
-    items = api.open_decks().items()
-    print(items)
-    if len(items) == 0:
-        print("Waiting for Stream Deck(s)...")
-        while len(items) == 0:
-            time.sleep(3)
-            items = api.open_decks().items()
-
-    api.render()
 
 if __name__ == "__main__":
-    start()
+    streamdecks = DeviceManager().enumerate()
+
+    print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
+
+    for index, deck in enumerate(streamdecks):
+        deck.open()
+        deck_id = deck.get_serial_number()
+        print(deck_id)
+        deck.reset()
+
+        print("Opened '{}' device (serial number: '{}')".format(deck.deck_type(), deck.get_serial_number()))
+
+        # Set initial screen brightness to 30%.
+        deck.set_brightness(30)
+
+        # Set initial key images.
+        for key in range(deck.key_count()):
+            update_key_image(deck, key, False)
+
+        # Register callback function for when a key state changes.
+        deck.set_key_callback(key_change_callback)
+
+        # Wait until all application threads have terminated (for this example,
+        # this is when all deck handles are closed).
+        for t in threading.enumerate():
+            try:
+                t.join()
+            except RuntimeError:
+                pass
